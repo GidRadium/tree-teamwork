51f1f780be64e1dac2b7cfb0f8bd2a8d97e2a709
Dariy Anpilogov <gidradiumcode@gmail.com>
2026-03-10 19:32:59 +0300

    Add CMakeLists.txt

--- src/BST/BST.h
+++ src/BST/BST.h
@@ -7,9 +7,12 @@ typedef struct BST BST;
 
 // Allocates memory for tree with no elements in it.
 BST* bstCreate();
+
 // Inserts value to the tree. Allocates memory for it. Do nothing if BST already contains it.
 void bstInsert(BST* tree, int value);
+
 // Checks if value is in tree.
 bool bstContains(BST* tree, int value);
+
 // Free all allocated memory for tree and values in it.
 void bstFree(BST* tree);
