7891cb8bb56f3c8042fd8bd4ce94311057e4b093
Dariy Anpilogov <gidradiumcode@gmail.com>
2026-03-10 20:51:21 +0300

    Add more tests.

--- tests/testBST.c
+++ tests/testBST.c
@@ -1,11 +1,100 @@
 #include "BST.h"
 #include <assert.h>
 
-int main(void)
+void testCreateEmptyTree()
 {
     BST* tree = bstCreate();
-    bstInsert(tree, 1);
-    assert(bstContains(tree, 1));
+    assert(tree);
+    assert(!bstContains(tree, 42));
+
+    bstFree(tree);
+}
+
+void testSingleInsert()
+{
+    BST* tree = bstCreate();
+    assert(tree);
+
+    bstInsert(tree, 100);
+    assert(bstContains(tree, 100));
+    assert(!bstContains(tree, -100));
+    assert(!bstContains(tree, 0));
+
+    bstFree(tree);
+}
+
+void testDublicatedInsert()
+{
+    BST* tree = bstCreate();
+    assert(tree);
+
+    bstInsert(tree, 100);
+    assert(bstContains(tree, 100));
+    assert(!bstContains(tree, -100));
+    assert(!bstContains(tree, 0));
+    bstInsert(tree, 100);
+    assert(bstContains(tree, 100));
+    bstInsert(tree, -100);
+    assert(bstContains(tree, 100));
+    assert(bstContains(tree, -100));
+    assert(!bstContains(tree, 0));
+    bstInsert(tree, -100);
+    assert(bstContains(tree, -100));
+    assert(bstContains(tree, 100));
+
+    bstFree(tree);
+}
+
+void testMultipleGrowingInserts()
+{
+    int count = 100;
+
+    BST* tree = bstCreate();
+    assert(tree);
+
+    for (int i = 0; i < count; ++i) {
+        bstInsert(tree, i);
+    }
+
+    for (int i = 0; i < count; ++i) {
+        assert(bstContains(tree, i));
+    }
+
+    assert(!bstContains(tree, -1));
+    assert(!bstContains(tree, count));
+
+    bstFree(tree);
+}
+
+void testMultipleDiffrentInserts()
+{
+    int values[] = {1, 2, 3, -1, -2, 0, 4, -4};
+    int count = 8;
+
+    BST* tree = bstCreate();
+    assert(tree);
+
+    for (int i = 0; i < count; ++i) {
+        bstInsert(tree, values[i]);
+    }
+
+    for (int i = 0; i < count; ++i) {
+        assert(bstContains(tree, values[i]));
+    }
+
+    assert(!bstContains(tree, 5));
+    assert(!bstContains(tree, -5));
+
+    bstFree(tree);
+}
+
+int main(void)
+{
+    testCreateEmptyTree();
+    testSingleInsert();
+    testDublicatedInsert();
+    testMultipleGrowingInserts();
+    testMultipleDiffrentInserts();
 
     return 0;
 }
